69fcf534b3fd36ce18306f1952d165e76e00bf85
maria <maludantas11@gmail.com>
2024-08-31 03:38:42 -0300

    feat: index page fetching data from json file

--- load-data.js
+++ load-data.js
@@ -10,11 +10,9 @@ function fetchJSONData() {
 
         .then((data) => {
             const currentPage = window.location.pathname.split('/');
-            console.log(currentPage)
 
             data.forEach(item => {
                 if (item.page === currentPage[2] || (currentPage[1] === "aboutMe" && currentPage[2] === "")) {
-                    console.log(currentPage[1], currentPage[2])
                     loadPage(item);
                 }
             });
@@ -31,9 +29,41 @@ function loadPage(pageData) {
         case "":
             const headerNameElement = document.getElementById("name");
             const headerageNationalityElement = document.getElementById("age-nationality");
+            const infoBlockContainer = document.getElementById("body-container");
 
             headerNameElement.textContent = pageData["header-info"]["name"];
             headerageNationalityElement.textContent = pageData["header-info"]["age-nationality"];
+
+            pageData["info-block"].forEach((info) => {
+                const infoBlock = document.createElement("div");
+                infoBlock.classList.add("info-block");
+
+                const plaque = document.createElement("div");
+                plaque.classList.add("plaque");
+
+                const svgMarkup = `<svg class="left" width="24" height="40" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="16" height="32" x="4" y="4" rx="8" ry="8" style="fill:#F5F5F7; stroke-width:2px; stroke:#1D1D1F"/>
+                </svg>`;
+
+                const blockTitle = document.createElement("p");
+                blockTitle.classList.add("block-title");
+
+                const infoUnordList = document.createElement("ul");
+
+                info["content"].forEach((item) => {
+                    const contentText = document.createElement("li");
+                    contentText.textContent = item;
+                    infoUnordList.appendChild(contentText);
+                });
+
+                plaque.innerHTML = svgMarkup;
+                plaque.innerHTML = svgMarkup;
+
+                infoBlock.appendChild(plaque)
+                infoBlock.appendChild(blockTitle)
+                infoBlock.appendChild(infoUnordList)
+                infoBlockContainer.appendChild(infoBlock)
+            })
 
             break;
         
@@ -79,9 +109,9 @@ function loadPage(pageData) {
                 cmdBracket1.classList.add("brackets");
                 cmdBracket1.textContent = "[";
 
-                const svgMarkup = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#55FF8C" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
-                    </svg>`;
+                const svgMarkup1 = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#55FF8C" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+                </svg>`;
 
                 const cmdSymbol = document.createElement("p");
                 cmdSymbol.classList.add("symbol");
@@ -97,7 +127,7 @@ function loadPage(pageData) {
                 cmdBracket2.textContent = "]";
 
                 cmdLine.appendChild(cmdBracket1);
-                cmdText.innerHTML = svgMarkup;
+                cmdText.innerHTML = svgMarkup1;
                 cmdText.appendChild(cmdSymbol);
                 cmdText.appendChild(cmdCommand);
                 cmdLine.appendChild(cmdText);
