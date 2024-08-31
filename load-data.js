a54dc6f83d1d6693e25e4fad452d50b8840aa07b
maria <maludantas11@gmail.com>
2024-08-31 03:00:49 -0300

    test: troubleshooting

--- load-data.js
+++ load-data.js
@@ -26,8 +26,8 @@ function fetchJSONData() {
   
 function loadPage(pageData) {
     switch (pageData.page) {
-        case " ":
         case "index.html":
+        case " ":
             const headerNameElement = document.getElementById("name");
             const headerageNationalityElement = document.getElementById("age-nationality");
 
